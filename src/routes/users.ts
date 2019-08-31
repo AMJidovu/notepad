84b50db1398bd88fa9b00cf1f4f90d975e24b0f7
Dante Issaias <dante@issaias.net>
2019-08-31 23:29:07 +0100

    cleanup
    Signed-off-by: Dante Issaias <dante@issaias.net>

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -7,19 +7,21 @@ import { User } from '../models/User'
 export const users = Router()
 
 users.post('/', async ({ body }, res, next) => {
-  const password = await hash(body.password)
-
   try {
-    const user = await User.create({ ...body, id: uuid(), password })
-    res.status(201).send(user)
+    res.status(201).send(
+      await User.create({
+        ...body,
+        id: uuid(),
+        password: await hash(body.password),
+      }),
+    )
   } catch (error) {
-    if (error.name === 'SequelizeUniqueConstraintError') {
-      res
-        .status(400)
-        .send({ error: 'user.exists', message: 'The email is already in use' })
-    } else {
-      next(error)
-    }
+    error.name === 'SequelizeUniqueConstraintError'
+      ? res.status(400).send({
+          error: 'user.exists',
+          message: 'The email is already in use',
+        })
+      : next(error)
   }
 })
 
