db2893caa393c6773831cf33064c78112a866102
Dante Issaias <dante@issaias.net>
2019-09-01 01:15:22 +0100

    use express-validator to validate login
    Signed-off-by: Dante Issaias <dante@issaias.net>

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,25 +1,34 @@
 import { Router } from 'express'
 import { verify } from 'argon2'
+import { check, validationResult } from 'express-validator'
 
 import { User } from '../models/User'
 import { ERRORS } from '../constants'
 
 export const auth = Router()
 
-auth.post('/login', async ({ body }, res, next) => {
-  if (!body.email || !body.password) {
-    return res.status(400).send(ERRORS.MISSING_CREDENTIALS)
-  }
-
-  try {
-    const user = await User.findOne({ where: { email: body.email } })
-
-    if (user && (await verify(user.password, body.password))) {
-      return res.send({ auth: true })
+auth.post(
+  '/login',
+  // prettier-ignore
+  [
+    check('email').exists().isEmail(),
+    check('password').exists(),
+  ],
+  async (req, res, next) => {
+    if (!validationResult(req).isEmpty()) {
+      return res.status(400).send(ERRORS.MISSING_CREDENTIALS)
     }
 
-    res.status(400).send(ERRORS.INVALID_CREDENTIALS)
-  } catch (error) {
-    next(error)
-  }
-})
+    try {
+      const user = await User.findOne({ where: { email: req.body.email } })
+
+      if (user && (await verify(user.password, req.body.password))) {
+        return res.send({ auth: true })
+      }
+
+      res.status(400).send(ERRORS.INVALID_CREDENTIALS)
+    } catch (error) {
+      next(error)
+    }
+  },
+)
